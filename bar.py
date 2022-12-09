f1484cdfbb338a668192e942944da5e172f73990
alex <topki4uk@mail.ru>
2022-12-09 15:21:07 +0300

    Изменил вывод бара

--- bar.py
+++ bar.py
@@ -8,18 +8,25 @@ class Bar:
         self.length = length
         self.progress = 1
         self.percent = '\r0 '
-        self.bar_string = '[{:<{}}]'
-        self.progress_bar = self.bar_string.format('=' * int(self.progress / self.length * self.size), self.size)
+        self.bar_string = '[{:▭<{}}]'
+        self.progress_bar = self.bar_string.format('', self.size)
 
     def update(self):
         self.progress += 1
         self.percent = f'\r{str(round(self.progress / self.length * 100, 2))}% '
-        self.progress_bar = self.bar_string.format('=' * int(self.progress / self.length * self.size), self.size)
+        self.progress_bar = self.bar_string.format('█' * int(self.progress / self.length * self.size), self.size)
+
+    def reboot(self):
+        self.progress = 1
 
     def show(self):
         output_bar = self.percent+self.progress_bar
         sys.stdout.writelines(output_bar)
         sys.stdout.flush()
+        if round(self.progress / self.length * 100, 2) >= 100:
+            sys.stdout.writelines(' SUCCESS!')
+            sys.stdout.flush()
+            print()
         self.update()
 
 
--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import time
 
 
 def main():
-    n = 300
+    n = 200
     bar = Bar(50, n)
+    for i in range(n):
+        bar.show()
+        time.sleep(0.05)
+
+    bar.reboot()
+
     for i in range(n):
         bar.show()
         time.sleep(0.05)
